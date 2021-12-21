a729c25b1b11860fd55f374f710eed6cc96863a1
kisman <81470009+kisman2000@users.noreply.github.com>
2021-12-21 17:57:41 +0300

    kisman.cc b0.1.6.1 | Part #3 | Rewrote Criticals

--- src/main/java/com/kisman/cc/module/combat/Criticals.java
+++ src/main/java/com/kisman/cc/module/combat/Criticals.java
@@ -2,23 +2,20 @@ package com.kisman.cc.module.combat;
 
 import com.kisman.cc.Kisman;
 import com.kisman.cc.event.events.PacketEvent;
-import com.kisman.cc.module.Category;
-import com.kisman.cc.module.Module;
+import com.kisman.cc.module.*;
 import com.kisman.cc.settings.Setting;
-import me.zero.alpine.listener.EventHandler;
-import me.zero.alpine.listener.Listener;
-import net.minecraft.entity.item.EntityEnderCrystal;
-import net.minecraft.network.play.client.CPacketPlayer;
-import net.minecraft.network.play.client.CPacketUseEntity;
+import me.zero.alpine.listener.*;
+import net.minecraft.entity.*;
+import net.minecraft.network.play.client.*;
 
 public class Criticals extends Module {
-    private Setting mode = new Setting("Mode", this, Modes.Packet);
+    private Setting strict = new Setting("Strict", this, false);
     private Setting onlyKillaura = new Setting("OnlyKillAura", this, false);
 
     public Criticals() {
         super("Criticals", "", Category.COMBAT);
 
-        setmgr.rSetting(mode);
+        setmgr.rSetting(strict);
         setmgr.rSetting(onlyKillaura);
     }
 
@@ -32,43 +29,33 @@ public class Criticals extends Module {
 
     @EventHandler
     private final Listener<PacketEvent.Send> listener = new Listener<>(event -> {
-        if(event.getPacket() instanceof CPacketUseEntity && ((CPacketUseEntity) event.getPacket()).getAction() != CPacketUseEntity.Action.ATTACK && !(((CPacketUseEntity) event.getPacket()).getEntityFromWorld(mc.world) instanceof EntityEnderCrystal)) {
-            if((onlyKillaura.getValBoolean() && KillAura.instance.isToggled()) || !onlyKillaura.getValBoolean()) {
-                doCriticals(mode.getValString());
-            }
-        }
-    });
+        if(event.getPacket() instanceof CPacketUseEntity) {
+            CPacketUseEntity packet = (CPacketUseEntity) event.getPacket();
+            if(packet.action.equals(CPacketUseEntity.Action.ATTACK) && mc.player.onGround && !mc.player.isInLava() && !mc.player.isInWater() && !mc.player.isInWeb) {
+                if(onlyKillaura.getValBoolean()) {
+                    if(!KillAura.instance.isToggled()) return;
+                }
 
-    private void doCriticals(String mode) {
-        if (!mode.equals("None")) {
-            if (!mc.player.onGround) {
-                return;
-            }
-            if (mc.player.isInWater() || mc.player.isInLava()) {
-                return;
-            }
-            if (mode.equals("Packet")) {
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + Double.longBitsToDouble(Double.doubleToLongBits(257.1226152887829) ^ 0x7FC011F63B72F4FFL), mc.player.posZ, true));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + Double.longBitsToDouble(Double.doubleToLongBits(247433.60726351582) ^ 0x7FE925D8A756DFC6L), mc.player.posZ, false));
-                mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
-            } else if (mode.equals("Bypass")) {
-                mc.player.motionY = Double.longBitsToDouble(Double.doubleToLongBits(98.57642382264255) ^ 0x7FE13D7D80BEB8A5L);
-                mc.player.fallDistance = Float.intBitsToFloat(Float.floatToIntBits(178.62473f) ^ 0x7EFE5323);
-                mc.player.onGround = false;
-            } else {
-                mc.player.jump();
-                if (mode.equals("MiniJump")) {
-                    mc.player.motionY /= Double.longBitsToDouble(Double.doubleToLongBits(0.9448694671300228) ^ 0x7FEE3C5EE489FF24L);
+                Entity entity = packet.getEntityFromWorld(mc.world);
+
+                if(entity instanceof EntityLivingBase) {
+                    double x = mc.player.posX,
+                            y = mc.player.posY,
+                            z = mc.player.posZ;
+
+                    if(strict.getValBoolean()) {
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(x, y + 0.07, z, false));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(x, y + 0.08, z, false));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(x, y, z, false));
+                    }
+
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(x, y + 0.05, z, false));
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(x, y, z, false));
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(x, y + 0.012, z, false));
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(x, y, z, false));
+                    mc.player.onCriticalHit(entity);
                 }
             }
         }
-    }
-
-    public enum Modes {
-        Packet,
-        Jump,
-        MiniJump,
-        Bypass
-    }
+    });
 }
