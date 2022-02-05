da56ee4fa5131d53964d8ab56bcf5f6b367081d8
PunCakeCat <tymurkosterin@gmail.com>
2022-02-05 15:24:44 +0200

    kisman.ccb0.1.6.0 | packet chat v2

--- src/main/java/com/kisman/cc/hud/hudmodule/render/PacketChat.java
+++ src/main/java/com/kisman/cc/hud/hudmodule/render/PacketChat.java
@@ -16,49 +16,37 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class PacketChat extends HudModule {
+
+    public static PacketChat Instance;
+
     private final TimerUtils timer = new TimerUtils();
     private int sliderWidth = 200;
     private int sliderHeight = 20;
+    private int width = 200;
 
     private final String header = "PacketChat";
     private final String stringWithMaxLength = "Cooldown";
     private double borderOffset = 5;
 
-    private Log logs = new Log();
+    public Log logs = new Log();
 
     public PacketChat() {
         super("PacketChat", "", HudCategory.PLAYER);
-
+        logs.messages.add(new Message("lol"));
+        logs.messages.add(new Message("lmao"));
+        Instance = this;
     }
 
     public void update() {
         setX(3);
         setY(HUD.instance.indicY.getValInt() + 8);
-
-        switch (HUD.instance.indicThemeMode.getValString()) {
-            case "Default": {
-                setW(sliderWidth + 4);
-                setH(6 + getHeight() + 4 * (getHeight() + sliderHeight + 3));
-                break;
-            }
-            case "Rewrite": {
-                setW(borderOffset * 3 + CustomFontUtil.getStringWidth(stringWithMaxLength) * 2);
-                setH(borderOffset * 7 + CustomFontUtil.getFontHeight() * 5);
-                break;
-            }
-        }
+        setW(width + 4);
+        setH(borderOffset * 7 + CustomFontUtil.getFontHeight() * 5);
     }
 
     @SubscribeEvent
     public void onRender(RenderGameOverlayEvent.Text event) {
-        switch (HUD.instance.indicThemeMode.getValString()) {
-            case "Default":
-                drawDefault();
-                break;
-            case "Rewrite":
-                drawRewrite();
-                break;
-        }
+        drawRewrite();
     }
 
     private void drawRewrite() {
@@ -100,78 +88,14 @@ public class PacketChat extends HudModule {
         //draw header
         CustomFontUtil.drawCenteredStringWithShadow(header, x + width / 2, y + borderOffset, ColorUtils.astolfoColors(100, 100));
 
-        //draw cooldown
-        CustomFontUtil.drawStringWithShadow("Cooldown", x + borderOffset, y + borderOffset * 3 + CustomFontUtil.getFontHeight(), ColorUtils.astolfoColors(100, 100));
-        count++;
 
-        //draw hurttime
-        CustomFontUtil.drawStringWithShadow("HurtTime", x + borderOffset, y + borderOffset * 3 + CustomFontUtil.getFontHeight() + (offset * count), ColorUtils.astolfoColors(100, 100));
-        count++;
-
-        //draw speed
-        CustomFontUtil.drawStringWithShadow("Speed", x + borderOffset, y + CustomFontUtil.getFontHeight() + (offset * count), ColorUtils.astolfoColors(100, 100));
-        count++;
-
+        //draws messages
         for(Message message : logs.messages)
         {
             CustomFontUtil.drawStringWithShadow(message.message, x + borderOffset, y + CustomFontUtil.getFontHeight() + (offset * count), ColorUtils.astolfoColors(100, 100));
             count++;
         }
 
-    }
-
-    private void drawSlider(double x, double y, double sliderWidth, double sliderHeight) {
-        if(HUD.instance.indicShadowSliders.getValBoolean()) Render2DUtil.drawShadowSliders(x, y, sliderWidth, sliderHeight, ColorUtils.astolfoColors(100, 100), 1);
-        else Render2DUtil.drawRect(x, y, x + sliderWidth, y + sliderHeight, ColorUtils.astolfoColors(100, 100));
-    }
-
-    private void drawDefault() {
-        double x = getX();
-        double y = getY();
-        double width = getW();
-        double height = getH();
-        int offset = 0;
-        int offsetHeight = getHeight() + sliderHeight + 3;
-
-        double prevX = mc.player.posX - mc.player.prevPosX;
-        double prevZ = mc.player.posZ - mc.player.prevPosZ;
-        double lastDist = Math.sqrt(prevX * prevX + prevZ * prevZ);
-        double currSpeed = lastDist * 15.3571428571D / 4;
-
-
-        //draw background
-        Render2DUtil.drawRect(x + 3, y + 3, x + width + 3, y + height - 3, (ColorUtils.getColor(33, 33, 42)));
-        Render2DUtil.drawRect(x + 3, y, x + width + 3, y + height, (ColorUtils.getColor(33, 33, 42)));
-        Render2DUtil.drawRect(x + 2, y + 2, x + width + 2, y + height - 2, (ColorUtils.getColor(45, 45, 55)));
-        Render2DUtil.drawRect(x + 2, y, x + width + 2, y + height, (ColorUtils.getColor(45, 45, 55)));
-        Render2DUtil.drawRect(x + 1, y + 1, x + width + 1, y + height - 1, (ColorUtils.getColor(60, 60, 70)));
-        Render2DUtil.drawRect(x + 1, y, x + width + 1, y + height, (ColorUtils.getColor(60, 60, 70)));
-        Render2DUtil.drawRect(x - 3, y - 8, x + width + 3, y + height - 3, (ColorUtils.getColor(33, 33, 42)));
-        Render2DUtil.drawRect(x - 3, y, x + width + 3, y + height, (ColorUtils.getColor(33, 33, 42)));
-        Render2DUtil.drawRect(x - 2, y - 7, x + width + 2, y + height - 2, (ColorUtils.getColor(45, 45, 55)));
-        Render2DUtil.drawRect(x - 2, y, x + width + 2, y + height, (ColorUtils.getColor(45, 45, 55)));
-        Render2DUtil.drawRect(x - 1, y - 6, x + width + 1, y + height - 1, (ColorUtils.getColor(60, 60, 70)));
-        Render2DUtil.drawRect(x - 1, y, x + width + 1, y + height, (ColorUtils.getColor(60, 60, 70)));
-        Render2DUtil.drawRect(x, y - 5, x + width, y + height, (ColorUtils.astolfoColors(100, 100)));
-        Render2DUtil.drawRect(x - 3, y - 1, x + width + 3, y + height + 3, (ColorUtils.getColor(33, 33, 42)));
-        Render2DUtil.drawRect(x - 2, y - 2, x + width + 2, y + height + 2, (ColorUtils.getColor(45, 45, 55)));
-        Render2DUtil.drawRect(x - 1, y - 3, x + width + 1, y + height + 1, (ColorUtils.getColor(60, 60, 70)));
-        Render2DUtil.drawRect(x, y - 4, x + width, y + height, (ColorUtils.getColor(34, 34, 40)));
-
-        //draw header
-        drawStringWithShadow(header, x + (width / 2 - getStringWidth(header) / 2), y + 2, ColorUtils.astolfoColors(100, 100));
-        offset += getHeight() + 6;
-
-        //draw cooldown
-        drawStringWithShadow("Cooldown", x + 2, y + offset, ColorUtils.astolfoColors(100, 100));
-        offset += offsetHeight;
-
-        //draw hurttime
-        drawStringWithShadow("HurtTime", x + 2, y + offset, ColorUtils.astolfoColors(100, 100));
-        offset += offsetHeight;
-
-        //draw health
-        drawStringWithShadow("Health", x + 2, y + offset, ColorUtils.astolfoColors(100, 100));
     }
 
     private void drawStringWithShadow(String text, double x, double y, int color) {
