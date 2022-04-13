a5673e35d6f53767371f0e4309e1fbdc3077759f
kisman <81470009+kisman2000@users.noreply.github.com>
2022-04-13 16:25:37 +0300

    kisman.cc b0.1.6.3 | Remove abstract mixins

--- src/main/java/com/kisman/cc/mixin/mixins/MixinNetworkManager.java
+++ src/main/java/com/kisman/cc/mixin/mixins/MixinNetworkManager.java
@@ -10,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = NetworkManager.class, priority = 10000)
-public abstract class MixinNetworkManager {
-    @Shadow public abstract boolean isChannelOpen();
+public class MixinNetworkManager {
+    @Shadow public boolean isChannelOpen() {return false;}
 
     @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;)V", at = @At("HEAD"), cancellable = true)
     private void onSendPacket(Packet<?> p_Packet, CallbackInfo callbackInfo) {
