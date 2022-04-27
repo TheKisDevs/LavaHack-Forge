6afcaff89ad980fdb4d765d5a4ffecf5f47106b9
Cuubicc <cubicmusic777@gmail.com>
2022-04-27 20:10:51 +0200

    fixed bugs in Rendering

--- src/main/java/com/kisman/cc/module/render/SelfCityESP.java
+++ src/main/java/com/kisman/cc/module/render/SelfCityESP.java
@@ -4,9 +4,7 @@ import com.kisman.cc.friend.FriendManager;
 import com.kisman.cc.module.Category;
 import com.kisman.cc.module.Module;
 import com.kisman.cc.settings.Setting;
-import com.kisman.cc.settings.util.BoxRendererPattern;
 import com.kisman.cc.util.Colour;
-import com.kisman.cc.util.MathUtil;
 import com.kisman.cc.util.Rendering;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
@@ -17,7 +15,9 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -35,6 +35,8 @@ public class SelfCityESP extends Module {
     private final Setting surroundCheck = register(new Setting("SurroundCheck", this, false));
     private final Setting terrain = register(new Setting("Terrain", this, false));
     private final Setting color = register(new Setting("Color", this, "Color", new Colour(255, 255, 255)));
+    private final Setting color1 = register(new Setting("Color1", this, "Color1", new Colour(255, 255, 255)));
+    private final Setting renderMode = register(new Setting("RenderMode", this, "Both", Arrays.asList("Box", "Outline", "Both", "Gradient", "Glow")));
     private final Setting lineWidth = register(new Setting("LineWidth", this, 2.0, 1.0, 5.0, false));
 
     public SelfCityESP(){
@@ -75,7 +77,23 @@ public class SelfCityESP extends Module {
 
         for(BlockPos pos : positions){
             AxisAlignedBB axisAlignedBB = Rendering.correct(new AxisAlignedBB(pos));
-            Rendering.draw(axisAlignedBB, lineWidth.getValFloat(), color.getColour(), Rendering.DUMMY_COLOR, Rendering.Mode.BOTH);
+            switch (renderMode.getValString()){
+                case "Box":
+                    Rendering.draw(axisAlignedBB, lineWidth.getValFloat(), color.getColour(), color1.getColour(), Rendering.Mode.BOX);
+                    break;
+                case "Outline":
+                    Rendering.draw(axisAlignedBB, lineWidth.getValFloat(), color.getColour(), color1.getColour(), Rendering.Mode.OUTLINE);
+                    break;
+                case "Both":
+                    Rendering.draw(axisAlignedBB, lineWidth.getValFloat(), color.getColour(), color1.getColour(), Rendering.Mode.BOTH);
+                    break;
+                case "Gradient":
+                    Rendering.draw(axisAlignedBB, lineWidth.getValFloat(), color.getColour(), color1.getColour(), Rendering.Mode.BOTH_GRADIENT);
+                    break;
+                case "Glow":
+                    Rendering.draw(axisAlignedBB, lineWidth.getValFloat(), color.getColour(), color1.getColour().withAlpha(0), Rendering.Mode.GRADIENT_CUSTOM_OUTLINE);
+                    break;
+            }
         }
     }
 
@@ -247,8 +265,8 @@ public class SelfCityESP extends Module {
         if(isPlayerNotInArea(pos))
             positions.add(pos);
          */
-        double x = MathUtil.roundHalf(mc.player.posX);
-        double z = MathUtil.roundHalf(mc.player.posZ);
+        double x = mc.player.posX;
+        double z = mc.player.posZ;
 
         BlockPos playerPos = new BlockPos(mc.player.posX, mc.player.posY, mc.player.posZ);
 
@@ -256,8 +274,15 @@ public class SelfCityESP extends Module {
 
         double diff;
 
-        pos = playerPos.north();
-        diff = diff(pos.getZ(), z);
+        north : {
+            pos = playerPos.north();
+            if(isReplaceable(pos))
+                break north;
+            diff = diff(pos.getZ(), z);
+            if(diff > 1.3)
+                positions.add(pos);
+        }
+        /*
         if(diff > 1.0){
             if(diff > 1.3)
                 positions.add(pos);
@@ -265,42 +290,42 @@ public class SelfCityESP extends Module {
             if(diff > 0.3)
                 positions.add(pos);
         }
+         */
 
-        pos = playerPos.east();
-        diff = diff(pos.getX(), x);
-        if(diff > 1.0){
+        east : {
+            pos = playerPos.east();
+            if(isReplaceable(pos))
+                break east;
+            diff = diff(pos.getX(), x);
             if(diff > 1.3)
-                positions.add(pos);
-        } else {
-            if(diff > 0.3)
                 positions.add(pos);
         }
 
-        pos = playerPos.south();
-        diff = diff(pos.getZ(), z);
-        if(diff > 1.0){
+        south : {
+            pos = playerPos.south();
+            if(isReplaceable(pos))
+                break south;
+            diff = diff(pos.getZ(), z);
             if(diff > 1.3)
-                positions.add(pos);
-        } else {
-            if(diff > 0.3)
                 positions.add(pos);
         }
 
-        pos = playerPos.west();
-        diff = diff(pos.getX(), x);
-        if(diff > 1.0){
+        west : {
+            pos = playerPos.west();
+            if(isReplaceable(pos))
+                break west;
+            diff = diff(pos.getX(), x);
             if(diff > 1.3)
-                positions.add(pos);
-        } else {
-            if(diff > 0.3)
                 positions.add(pos);
         }
     }
 
+    public boolean isReplaceable(BlockPos pos){
+        return blockState(pos).getBlock().isReplaceable(mc.world, pos);
+    }
+
     public double diff(double a, double b){
-        double max = Math.max(a, b);
-        double min = Math.min(a, b);
-        return max - min;
+        return Math.abs(a - b);
     }
 
     public boolean isPlayerNotInArea(BlockPos pos){
