a84009231962541be83bff03646e552d5634d645
Cuubicc <cubicmusic777@gmail.com>
2022-05-14 09:58:42 +0200

    added stuff

---
+++ src/main/java/com/kisman/cc/util/thread/Func.java
@@ -0,0 +1,7 @@
+package com.kisman.cc.util.thread;
+
+public interface Func<R> {
+
+    R call(Object... o);
+
+}
---
+++ src/main/java/com/kisman/cc/util/thread/QueuedThreadExecutor.java
@@ -0,0 +1,38 @@
+package com.kisman.cc.util.thread;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+@Deprecated
+public final class QueuedThreadExecutor {
+
+    private final Queue<Func<?>> executionQueue;
+
+    private final Queue<Runnable> pendingExecutions;
+
+    private ExecutorService executor;
+
+    public QueuedThreadExecutor(){
+        this.executionQueue = new LinkedList<>();
+        this.pendingExecutions = new LinkedList<>();
+        this.executor = new ScheduledThreadPoolExecutor(1);
+    }
+
+    public void submit(Func<?> func){
+        executionQueue.offer(func);
+    }
+
+    public void runNext(Object... args){
+        if(executionQueue.isEmpty()) return;
+        Func<?> func = executionQueue.poll();
+        pendingExecutions.offer(() -> func.call(args));
+        if(!executor.isShutdown() || !executor.isTerminated()) return;
+        executor.submit(() -> {
+            while(!pendingExecutions.isEmpty()){
+                pendingExecutions.poll().run();
+            }
+        });
+    }
+}
