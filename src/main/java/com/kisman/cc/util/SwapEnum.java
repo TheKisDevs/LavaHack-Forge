7732aebdea6915d1e9e51c2f8f0c61e361fcab15
Cuubicc <cubicmusic777@gmail.com>
2022-05-28 09:05:42 +0200

    dynamic swap enum

---
+++ src/main/java/com/kisman/cc/util/SwapEnum.java
@@ -0,0 +1,51 @@
+package com.kisman.cc.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.play.client.CPacketHeldItemChange;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.spi.AbstractLogger;
+import org.cubic.dynamictask.AbstractEnum;
+import org.cubic.dynamictask.AbstractTask;
+
+import java.io.FileDescriptor;
+
+public class SwapEnum extends AbstractEnum<SwapEnum, AbstractTask<Void>> {
+
+    private static final Minecraft mc = Minecraft.getMinecraft();
+
+    private static final Class<Void> retType = Void.class;
+
+    private static final Class<?>[] types = {Integer.class, Boolean.class};
+
+    public static final SwapEnum NORMAL = new SwapEnum("Normal", AbstractTask.types(retType, types).task(arg -> {
+        int slot = arg.fetch(0);
+        boolean swapBack = arg.fetch(1);
+        if(swapBack)
+            return null;
+        mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
+        mc.player.inventory.currentItem = slot;
+        return null;
+    }));
+
+    public static final SwapEnum SILENT = new SwapEnum("Silent", AbstractTask.types(retType, types).task(arg -> {
+        int slot = arg.fetch(0);
+        mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
+        mc.player.inventory.currentItem = slot;
+        return null;
+    }));
+
+    public static final SwapEnum PACKET = new SwapEnum("Packet", AbstractTask.types(retType, types).task(arg -> {
+        int slot = arg.fetch(0);
+        mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
+        return null;
+    }));
+
+    public SwapEnum(String name, AbstractTask<Void> task) {
+        super(name, task);
+    }
+
+    @Override
+    public AbstractEnum<SwapEnum, AbstractTask<Void>>[] values() {
+        return new SwapEnum[]{NORMAL, SILENT, PACKET};
+    }
+}
---
+++ src/main/java/com/kisman/cc/util/SwapEnum2.java
@@ -0,0 +1,41 @@
+package com.kisman.cc.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.play.client.CPacketHeldItemChange;
+import org.cubic.dynamictask.AbstractTask;
+
+public class SwapEnum2 {
+
+    private static final Minecraft mc = Minecraft.getMinecraft();
+
+    private static final AbstractTask.DelegateAbstractTask<Void> task = AbstractTask.types(Void.class, Integer.class, Boolean.class);
+
+    public enum Swap {
+        Normal(task.task(arg -> {
+            if(arg.fetch(1))
+                return null;
+            mc.player.connection.sendPacket(new CPacketHeldItemChange(arg.fetch(0)));
+            mc.player.inventory.currentItem = arg.fetch(0);
+            return null;
+        })),
+        Silent(task.task(arg -> {
+            mc.player.connection.sendPacket(new CPacketHeldItemChange(arg.fetch(0)));
+            mc.player.inventory.currentItem = arg.fetch(1);
+            return null;
+        })),
+        Packet(task.task(arg -> {
+            mc.player.connection.sendPacket(new CPacketHeldItemChange(arg.fetch(0)));
+            return null;
+        }));
+
+        private final AbstractTask<Void> abstractTask;
+
+        Swap(AbstractTask<Void> task) {
+            this.abstractTask = task;
+        }
+
+        public AbstractTask<Void> getTask(){
+            return abstractTask;
+        }
+    }
+}
