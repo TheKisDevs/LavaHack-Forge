72075071cdd7d30b1f2ca57c896240f277f355bf
Cuubicc <cubicmusic777@gmail.com>
2022-08-01 16:43:27 +0200

    cubic gl

---
+++ src/main/java/com/kisman/cc/util/render/cubicgl/CubicGL.java
@@ -0,0 +1,108 @@
+package com.kisman.cc.util.render.cubicgl;
+
+import com.kisman.cc.util.render.Rendering;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import org.cubic.dynamictask.AbstractTask;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.*;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CubicGL {
+
+    public static final int CGL_SHADE_MODEL_FLAT = 0;
+    public static final int CGL_SHADE_MODEL_SMOOTH = 1;
+
+    public static void scissors(int x1, int y1, int x2, int y2){
+        int mx = Math.min(x1, x2);
+        int my = Math.min(y1, y2);
+        int dx = Math.abs(x1 - x2);
+        int dy = Math.abs(y1 - y2);
+        GL11.glScissor(mx, my, dx, dy);
+    }
+
+    public static void scissorsNormalized(double x1, double y1, double x2, double y2){
+        scissors((int) (x1 * Display.getWidth()), (int) (y1 * Display.getHeight()), (int) (x2 * Display.getWidth()), (int) (y2 * Display.getHeight()));
+    }
+
+    public static void scissorNormalizedCenter(double x1, double y1, double x2, double y2){
+        scissorsNormalized((x1 + 1.0) / 2.0, (y1 + 1.0) / 2.0, (x2 + 1.0) / 2.0, (y2 + 1.0) / 2.0);
+    }
+
+    public static void setup(){
+        Rendering.setup();
+    }
+
+    public static void prepare(){
+        Rendering.prepare();
+    }
+
+    public static void restore(){
+        Rendering.restore();
+    }
+
+    public static void release(){
+        Rendering.restore();
+    }
+
+    public static void drawRect(int x1, int y1, int x2, int y2, Color color){
+        drawRect(CGL_SHADE_MODEL_FLAT, DisplayVertex2D.of(x1, y1, x2, y2), color);
+    }
+
+    public static void drawRect(int shadeModel, int x1, int y1, int x2, int y2, Color color){
+        drawRect(shadeModel, DisplayVertex2D.of(x1, y1, x2, y2), color);
+    }
+
+    public static void drawRect(DisplayVertex2D vertex2D, Color color){
+        drawRect(CGL_SHADE_MODEL_FLAT, vertex2D, color);
+    }
+
+    public static void drawRect(int shadeModel, DisplayVertex2D vertex2D, Color color){
+        run("shadeModelBegin", shadeModel);
+        GL11.glColor4f(color.getRed() / 255.0f, color.getGreen() / 255.0f, color.getBlue() / 255.0f, color.getAlpha() / 255.0f);
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder buf = tessellator.getBuffer();
+        buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
+        buf.pos(vertex2D.getX1(), vertex2D.getY1(), 0);
+        buf.pos(vertex2D.getX2(), vertex2D.getY1(), 0);
+        buf.pos(vertex2D.getX2(), vertex2D.getY2(), 0);
+        buf.pos(vertex2D.getX1(), vertex2D.getY2(), 0);
+        tessellator.draw();
+        run("shadeModelEnd", shadeModel);
+    }
+
+    private static final Map<String, Map<Integer, AbstractTask<Integer>>> tasks = new ConcurrentHashMap<>();
+
+    private static int run(String type, Integer mode, Object... args){
+        return tasks.get(type).get(mode).doTask(args);
+    }
+
+    static {
+        Map<Integer, AbstractTask<Integer>> shadeModelBeginMap = new ConcurrentHashMap<>();
+        shadeModelBeginMap.put(CGL_SHADE_MODEL_FLAT, AbstractTask.types(Integer.class).task(arg -> {
+            setup();
+            return 0;
+        }));
+        shadeModelBeginMap.put(CGL_SHADE_MODEL_SMOOTH, AbstractTask.types(Integer.class).task(arg -> {
+            setup();
+            prepare();
+            return 0;
+        }));
+        tasks.put("shadeModelBegin", shadeModelBeginMap);
+        Map<Integer, AbstractTask<Integer>> shadeModelEndMap = new ConcurrentHashMap<>();
+        shadeModelEndMap.put(CGL_SHADE_MODEL_FLAT, AbstractTask.types(Integer.class).task(arg -> {
+            release();
+            return 0;
+        }));
+        shadeModelEndMap.put(CGL_SHADE_MODEL_SMOOTH, AbstractTask.types(Integer.class).task(arg -> {
+            restore();
+            release();
+            return 0;
+        }));
+        tasks.put("shadeModelEnd", shadeModelEndMap);
+    }
+}
