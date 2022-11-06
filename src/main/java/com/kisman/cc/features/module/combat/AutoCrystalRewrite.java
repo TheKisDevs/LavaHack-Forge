051bef2056038acc1cd95fecb56d2057a60a6715
Cuubicc <cubicmusic777@gmail.com>
2022-11-06 21:35:00 +0100

    AutoCrystalRewrite can now calculate place

--- src/main/java/com/kisman/cc/features/module/combat/AutoCrystalRewrite.java
+++ src/main/java/com/kisman/cc/features/module/combat/AutoCrystalRewrite.java
@@ -5,6 +5,7 @@ import com.kisman.cc.features.module.Module;
 import com.kisman.cc.settings.Setting;
 import com.kisman.cc.settings.types.SettingEnum;
 import com.kisman.cc.util.entity.EntityUtil;
+import com.kisman.cc.util.enums.dynamic.SwapEnum2;
 import com.kisman.cc.util.manager.friend.FriendManager;
 import com.kisman.cc.util.world.CrystalUtils;
 import net.minecraft.entity.player.EntityPlayer;
@@ -16,16 +17,78 @@ public class AutoCrystalRewrite extends Module {
     private final SettingEnum<Safety> safety = new SettingEnum<>("Safety", this, Safety.None).register();
     private final Setting safetyBalance = register(new Setting("SafetyBalance", this, 2, 0, 10, false));
 
+    private final SettingEnum<SwapEnum2.Swap> swap = new SettingEnum<>("Switch", this, SwapEnum2.Swap.None).register();
+    private final Setting swapDelay = register(new Setting("SwapDelay", this, 0, 0, 10, false));
+    private final SettingEnum<SwapEnum2.Swap> antiWeakness = new SettingEnum<>("AntiWeakness", this, SwapEnum2.Swap.None);
+
+    private final SettingEnum<TargetMode> targetMode = new SettingEnum<>("TargetMode", this, TargetMode.Closest).register();
+    private final Setting targetRange = register(new Setting("TargetRange", this, 12, 1, 16, false));
+
     private final Setting placeRange = register(new Setting("PlaceRange", this, 5, 0, 6, false));
     private final Setting placeWallRange = register(new Setting("PlaceWallRange", this, 3, 0, 6, false));
     private final Setting firePlace = register(new Setting("FirePlace", this, false));
     private final Setting terrain = register(new Setting("Terrain", this, false));
 
-    private final SettingEnum<TargetMode> targetMode = new SettingEnum<>("TargetMode", this, TargetMode.Closest).register();
-    private final Setting targetRange = register(new Setting("TargetRange", this, 12, 1, 16, false));
+    private final Setting minPlaceDamage = register(new Setting("MinPlaceDamage", this, 5, 0, 36, false));
+    private final Setting maxSelfPlace = register(new Setting("MaxSelfPlace", this, 8, 0, 36, false));
 
     public AutoCrystalRewrite(){
         super("AutoCrystalRewrite", Category.COMBAT);
+    }
+
+    private EntityPlayer target;
+
+    private PositionInfo calculatePlace(){
+        if(target == null)
+            return null;
+
+        PositionInfo positionInfo = new PositionInfo(BlockPos.ORIGIN, -1, -1);
+
+        for(BlockPos pos : CrystalUtils.getSphere(placeRange.getValFloat(), true, false)){
+
+            if(
+                    !isPosInRange(pos)
+                    || !CrystalUtils.canPlaceCrystal(
+                            pos,
+                            true,
+                            true,
+                            false,
+                            firePlace.getValBoolean()
+                    )
+            ){
+                continue;
+            }
+
+            double damage = CrystalUtils.calculateDamage(
+                    mc.world,
+                    pos.getX() + 0.5,
+                    pos.getY() + 1,
+                    pos.getZ() + 0.5,
+                    target,
+                    terrain.getValBoolean()
+            );
+
+            if(damage < minPlaceDamage.getValDouble())
+                continue;
+
+            double selfDamage = CrystalUtils.calculateDamage(
+                    mc.world,
+                    pos.getX() + 0.5,
+                    pos.getY() + 1,
+                    pos.getZ() + 0.5,
+                    mc.player,
+                    terrain.getValBoolean()
+            );
+
+            if(selfDamage > maxSelfPlace.getValDouble())
+                continue;
+
+            PositionInfo place = new PositionInfo(pos, damage, selfDamage);
+
+            positionInfo = positionInfo.max(place);
+        }
+
+        return positionInfo.targetDamage < 0 ? null : positionInfo;
     }
 
     private double getSafetyDamage(double damage, double selfDamage){
@@ -147,5 +210,36 @@ public class AutoCrystalRewrite extends Module {
         Closest,
         Health,
         Damage
+    }
+
+    private static class PositionInfo {
+
+        private final BlockPos blockPos;
+
+        private final double targetDamage;
+
+        private final double selfDamage;
+
+        public PositionInfo(BlockPos blockPos, double targetDamage, double selfDamage) {
+            this.blockPos = blockPos;
+            this.targetDamage = targetDamage;
+            this.selfDamage = selfDamage;
+        }
+
+        public BlockPos getBlockPos() {
+            return blockPos;
+        }
+
+        public double getTargetDamage() {
+            return targetDamage;
+        }
+
+        public double getSelfDamage() {
+            return selfDamage;
+        }
+
+        public PositionInfo max(PositionInfo other){
+            return other.targetDamage > targetDamage ? other : this;
+        }
     }
 }
