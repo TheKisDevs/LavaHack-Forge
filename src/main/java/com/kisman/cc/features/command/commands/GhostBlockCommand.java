549662d2a5e206bd8ad963024a1ccaf3077dc4fc
Cuubicc <cubicmusic777@gmail.com>
2022-12-09 17:20:21 +0100

    added smart square mode to FlattenRewrite

---
+++ src/main/java/com/kisman/cc/features/command/commands/GhostBlockCommand.java
@@ -0,0 +1,39 @@
+package com.kisman.cc.features.command.commands;
+
+import com.kisman.cc.features.command.Command;
+import com.kisman.cc.util.enums.dynamic.BlockEnum;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+public class GhostBlockCommand extends Command {
+
+    public GhostBlockCommand(){
+        super("gb");
+    }
+
+    @Override
+    public String getDescription() {
+        return "DON'T USE THIS PLEASE";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "kek i said don't use it";
+    }
+
+    @Override
+    public void runCommand(@NotNull String s, @NotNull String[] args) {
+        if(args.length < 1){
+            error(getSyntax());
+            return;
+        }
+        Block block = Block.REGISTRY.getObject(new ResourceLocation(args[0]));
+        if(block == null){
+            error(getSyntax());
+            return;
+        }
+        mc.world.setBlockState(mc.objectMouseOver.getBlockPos().offset(mc.objectMouseOver.sideHit), block.getDefaultState());
+    }
+}
