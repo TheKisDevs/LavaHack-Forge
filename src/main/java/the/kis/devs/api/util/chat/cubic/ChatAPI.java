e0f75b88c8ce8660bd0f5fdbead7ae7d1d34ed25
kisman <81470009+kisman2000@users.noreply.github.com>
2022-12-19 17:22:09 +0300

    LavaHack b0.1.6.5-9 | Fixed crash by ChatAPI

--- src/main/java/the/kis/devs/api/util/chat/cubic/ChatAPI.java
+++ src/main/java/the/kis/devs/api/util/chat/cubic/ChatAPI.java
@@ -1,5 +1,6 @@
 package the.kis.devs.api.util.chat.cubic;
 
+import com.kisman.cc.features.module.Module;
 import com.kisman.cc.util.chat.cubic.AbstractChatMessage;
 import net.minecraft.util.text.ITextComponent;
 
@@ -12,6 +13,8 @@ public class ChatAPI extends AbstractChatMessageAPI {
     public ChatAPI(AbstractChatMessage sender) {this.sender = sender;}
     @Override public void printClientMessage(ITextComponent textComponent) {sender.printClientMessage(textComponent);}
     @Override public void printClientMessage(String message) {sender.printClientMessage(message);}
+    @Override public void printClientModuleMessage(String message, Module module) {sender.printClientModuleMessage(message, module);}
+    @Override public void printClientModuleMessage(ITextComponent textComponent, Module module) {sender.printClientModuleMessage(textComponent, module);}
     @Override public void printClientModuleMessage(ITextComponent textComponent) {sender.printClientModuleMessage(textComponent);}
     @Override public void printClientModuleMessage(String message) {sender.printClientModuleMessage(message);}
     @Override public void printModuleMessage(ITextComponent textComponent) {sender.printModuleMessage(textComponent);}
